436e9928ff94d694e7070b9dd8f53320868b3894
nirvanaflame <sergey.putik@mail.ru>
2021-07-10 18:07:37 +0200

    [codewars] Update test

--- src/test/js/codewars/mocha/july/SwapCase.js
+++ src/test/js/codewars/mocha/july/SwapCase.js
@@ -1,19 +1,28 @@
 const { assert } = require("chai")
 
+
 describe("String.prototype.toAlternatingCase", () => {
   it("should work for fixed asserts (provided in the description)", () => {
-    assert.assertEquals("hello world".toAlternatingCase(), "HELLO WORLD");
-    assert.assertEquals("HELLO WORLD".toAlternatingCase(), "hello world");
-    assert.assertEquals("hello WORLD".toAlternatingCase(), "HELLO world");
-    assert.assertEquals("HeLLo WoRLD".toAlternatingCase(), "hEllO wOrld");
-    assert.assertEquals("12345".toAlternatingCase(), "12345");
-    assert.assertEquals("1a2b3c4d5e".toAlternatingCase(), "1A2B3C4D5E");
-    assert.assertEquals("String.prototype.toAlternatingCase".toAlternatingCase(), "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
-    assert.assertEquals("Hello World".toAlternatingCase().toAlternatingCase(), "Hello World");
+    assert.strictEqual("hello world".toAlternatingCase(), "HELLO WORLD");
+    assert.strictEqual("HELLO WORLD".toAlternatingCase(), "hello world");
+    assert.strictEqual("hello WORLD".toAlternatingCase(), "HELLO world");
+    assert.strictEqual("HeLLo WoRLD".toAlternatingCase(), "hEllO wOrld");
+    assert.strictEqual("12345".toAlternatingCase(), "12345");
+    assert.strictEqual("1a2b3c4d5e".toAlternatingCase(), "1A2B3C4D5E");
+    assert.strictEqual("String.prototype.toAlternatingCase".toAlternatingCase(), "sTRING.PROTOTYPE.TOaLTERNATINGcASE");
+    assert.strictEqual("Hello World".toAlternatingCase().toAlternatingCase(), "Hello World");
   });
 });
 
 
+/**
+ * 
+ * altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+ * Define String.prototype.toAlternatingCase 
+ * such that each lowercase letter becomes uppercase 
+ * and each uppercase letter becomes lowercase
+ * 
+ */
 String.prototype.toAlternatingCase = function () {
-  this.map
-}
+  return this.split('').map(char => (char == char.toLowerCase()) ? char.toUpperCase() : char.toLowerCase()).join('')
+};
