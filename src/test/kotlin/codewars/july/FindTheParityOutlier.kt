0c6905f71f955e9041989125ef4c69203b162839
nirvanaflame <sergey.putik@mail.ru>
2021-07-12 23:42:30 +0200

    [fix] implicit boxing leads to performance hit

--- src/test/kotlin/codewars/july/FindTheParityOutlier.kt
+++ src/test/kotlin/codewars/july/FindTheParityOutlier.kt
@@ -14,31 +14,29 @@ class FindTheParityOutlier : DescribeSpec({
     describe("Find single odd or even value in an array") {
 
         it("should return 3 from [2, 6, 8, -10, 3]") {
-            find(arrayOf(2, 6, 8, -10, 3)) shouldBe 2
+            find(intArrayOf(2, 6, 8, -10, 3)) shouldBe 2
         }
 
         it("should return 206847684 from [206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781]") {
-            find(arrayOf(206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781)) shouldBe 206847684
+            find(intArrayOf(206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781)) shouldBe 206847684
         }
 
         it("should return 0") {
-            find(arrayOf(Integer.MAX_VALUE, 0, 1)) shouldBe 0
+            find(intArrayOf(Integer.MAX_VALUE, 0, 1)) shouldBe 0
         }
     }
 
     describe("Best solution from codewars") {
 
         it("should return 206847684 from [206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781]") {
-            findBestSolution(arrayOf(206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781)) shouldBe 206847684
-
-            val sad = intArrayOf(12321_123123.toInt())
+            findBestSolution(intArrayOf(206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781)) shouldBe 206847684
         }
     }
 })
 
 
-fun find(arr: Array<Int>): Int {
+fun find(arr: IntArray): Int {
     return arr.partition { it % 2 == 0 }.let { if (it.first.size == 1) it.first[0] else it.second[0] }
 }
 
-fun findBestSolution(arr: Array<Int>) = arr.singleOrNull { it % 2 == 0 } ?: arr.single { it % 2 == 1 }
+fun findBestSolution(arr: IntArray) = arr.singleOrNull { it % 2 == 0 } ?: arr.single { it % 2 == 1 }
