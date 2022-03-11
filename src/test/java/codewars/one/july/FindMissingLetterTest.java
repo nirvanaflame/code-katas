33c1bf95ce9f750846fcf05bb72351b238ac14ac
nf <sergey.putik@mail.ru>
2022-03-11 12:42:32 +0100

    Update date, AreTheySameTest

---
+++ src/test/java/codewars/one/july/FindMissingLetterTest.java
@@ -0,0 +1,53 @@
+package codewars.one.july;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Find the missing letter
+ * <p>
+ * Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+ * <p>
+ * You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+ * The array will always contain letters in only one case.
+ * <p>
+ * Example:
+ * <p>
+ * ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+ * <p>
+ * ["a","b","c","d","f"] -> "e"
+ * ["O","Q","R","S"] -> "P"
+ */
+class FindMissingLetterTest {
+
+    @Test
+    void shouldReturnMissingLetter() {
+        assertEquals('e', findMissingLetter(new char[]{'a', 'b', 'c', 'd', 'f'}));
+    }
+
+    @Test
+    void shouldReturnMissingLetterEffective() {
+        assertEquals('e', findMissingLetterEffective(new char[]{'a', 'b', 'c', 'd', 'f'}));
+    }
+
+    private char findMissingLetter(char[] chars) {
+        return (char) IntStream.range(chars[0], chars[chars.length - 1])
+                .filter(it -> !Arrays.toString(chars).contains(String.valueOf((char) it)))
+                .findFirst().getAsInt();
+    }
+
+    private char findMissingLetterEffective(char[] chars) {
+        AtomicInteger prev = new AtomicInteger(chars[0]);
+
+        return (char) (new String(chars).chars()
+                .filter(it -> it != prev.getAndIncrement())
+                .findFirst()
+                .getAsInt() - 1
+        );
+    }
+}
---
+++ src/test/java/codewars/one/july/FindTheUniqueNumberTest.java
@@ -0,0 +1,40 @@
+package codewars.one.july;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * There is an array with some numbers. All numbers are equal except for one. Try to find it!
+ * <p>
+ * Kata.findUniq(new double[]{ 1, 1, 1, 2, 1, 1 }); // => 2
+ * Kata.findUniq(new double[]{ 0, 0, 0.55, 0, 0 }); // => 0.55
+ * It’s guaranteed that array contains at least 3 numbers.
+ */
+class FindTheUniqueNumberTest {
+
+    @Test
+    void shouldReturnUniqueNumber() {
+        assertEquals(2.0, findUnique(new double[]{1, 1, 1, 2, 1, 1}));
+    }
+
+    @Test
+    void shouldReturnUniqueNumberForManyDuplicated() {
+        assertEquals(2.0, findUnique(new double[]{1, 1, 1, 2, 1, 1, 3, 3, 5, 5, 7, 8, 9, 8, 7, 9}));
+    }
+
+    private double findUnique(double[] array) {
+        HashMap<Double, Integer> countValues = new HashMap<>();
+        for (double v : array) {
+            if (countValues.get(v) == null) {
+                countValues.put(v, 1);
+            } else {
+                countValues.merge(v, 1, Integer::sum);
+            }
+
+        }
+        return countValues.entrySet().stream().filter(entry -> entry.getValue() == 1).findFirst().get().getKey();
+    }
+}
