b9b31b629ee148b7fe49a4bc77bd9212466e43a0
nf <sergey.putik@mail.ru>
2022-05-07 20:22:05 +0200

    {codewars} SplitStringAsCouple
    #regex lookbehind for 2 chars and split.

---
+++ src/test/java/codewars/two/may/SplitStringAsCoupleTest.java
@@ -0,0 +1,24 @@
+package codewars.two.may;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SplitStringAsCoupleTest {
+
+  @Test
+  public void testOddString() {
+    String s = "abcde";
+    String s1 = "LovePizza";
+    assertEquals("[ab, cd, e_]", Arrays.toString(splitAsCoupe(s)));
+    assertEquals("[Lo, ve, Pi, zz, a_]", Arrays.toString(splitAsCoupe(s1)));
+  }
+
+  private String[] splitAsCoupe(String str) {
+    String[] s = str.split("(?<=\\G..)");
+    if (str.length() % 2 != 0) s[s.length - 1] += "_";
+    return  s;
+  }
+}
