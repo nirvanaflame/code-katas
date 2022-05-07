e13c2899a60c500c8139d4dfeed1e050983dc1e3
nf <sergey.putik@mail.ru>
2022-05-07 19:42:25 +0200

    {codewars} Reverse words in sentence

---
+++ src/test/java/codewars/two/may/ReverseWordTest.java
@@ -0,0 +1,26 @@
+package codewars.two.may;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseWordTest {
+
+  @Test
+  public void exampleCases() {
+    assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", reverseWords("The quick brown fox jumps over the lazy dog."));
+    assertEquals("elppa", reverseWords("apple"));
+    assertEquals("a b c d", reverseWords("a b c d"));
+    assertEquals("elbuod  decaps  sdrow", reverseWords("double  spaced  words"));
+  }
+
+  private String reverseWords(String s) {
+    return Arrays.stream(s.split(" "))
+        .map(StringBuilder::new)
+        .map(StringBuilder::reverse)
+        .collect(Collectors.joining(" "));
+  }
+}
