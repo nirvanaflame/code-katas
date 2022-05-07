d9b1212b664c3fc5e0ae46832c3bcacb1d6e6179
nf <sergey.putik@mail.ru>
2022-05-07 18:56:41 +0200

    {codewars} Create Phone Number

---
+++ src/test/java/codewars/two/may/CreatePhoneNumberTest.java
@@ -0,0 +1,19 @@
+package codewars.two.may;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreatePhoneNumberTest {
+
+  @Test
+  void shouldReturnPhoneNumber() {
+    assertEquals("(123) 456-7890", createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+  }
+
+  private String createPhoneNumber(int[] a) {
+    return String.format("(%d%d%d) %d%d%d-%d%d%d%d", Arrays.stream(a).boxed().toArray());
+  }
+}
