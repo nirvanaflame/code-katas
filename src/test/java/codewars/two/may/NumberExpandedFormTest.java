3570865db9063a2c9c949a5987492eb276b98564
nf <sergey.putik@mail.ru>
2022-05-07 14:31:44 +0200

    {codewars} Number in Expanded Form

---
+++ src/test/java/codewars/two/may/NumberExpandedFormTest.java
@@ -0,0 +1,29 @@
+package codewars.two.may;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberExpandedFormTest {
+
+  @Test
+  public void testSomething() {
+    assertEquals("10 + 2", expandedForm(12));
+    assertEquals("40 + 2", expandedForm(42));
+    assertEquals("70000 + 300 + 4", expandedForm(70304));
+    assertEquals("9000000", expandedForm(9000000));
+  }
+
+  private String expandedForm(int n) {
+
+    String outs = "";
+    for (int i = 10; i < n; i *= 10) {
+      int rem = n % i;
+      outs = rem > 0 ? " + " + rem + outs : outs;
+      n -= rem;
+    }
+    outs = n + outs;
+
+    return outs;
+  }
+}
