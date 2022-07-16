bf66bf2fd7d77f3c66e0498269a39eccdbc8e48a
nf <sergey.putik@mail.ru>
2022-07-16 12:45:39 +0200

    Upgrade version

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    idea
+    java
+    groovy
+}
+
+group = "com.sputik"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "18"
+}
+
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
