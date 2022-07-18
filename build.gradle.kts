f52268c4f123fcfbb8d16f60db4a1ff8721737aa
nf <sergey.putik@mail.ru>
2022-07-18 10:55:10 +0200

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ java {
 }
 
 dependencies {
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 
@@ -30,7 +30,7 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.compileJava {
+tasks.compileTestJava {
     options.encoding = "UTF-8"
 }
 
